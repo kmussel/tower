dd97efc9ca56e0649a67a6847bd936e8e20be382
Kevin <kmussel@gmail.com>
2017-10-06 03:09:29 +0800

    update authorization to take a connection and not need a client id

--- lib/tower/grant_types/password.ex
+++ lib/tower/grant_types/password.ex
@@ -7,19 +7,23 @@ defmodule Tower.GrantType.Password do
 
   @repo Application.get_env(:tower, :repo)
   @resource_owner Application.get_env(:tower, :resource_owner)
-  
-  def authorize(%{"email" => email, "password" => password, "client_id" => client_id, "scopes" => scopes}) do
-    client = @repo.get_by(Tower.Models.OAuthApplication, uid: client_id)
-    owner = resource_owner_from_credentials(%{"email" => email, "password" => password})
+
+  def authorize(conn, %{"email" => _, "password" => _, "scopes" => scopes} = params) do
+    client =
+      case is_nil(params["client_id"]) do
+        true -> nil
+        false -> @repo.get_by(Tower.Models.OAuthApplication, uid: params["client_id"])
+      end
+    owner = resource_owner_from_credentials(conn, client)
     create_access_token(client, owner, scopes)
   end
 
-  def authorize(%{"email" => _, "password" => _, "client_id" => _} = params) do
+  def authorize(conn, %{"email" => _, "password" => _, "client_id" => _} = params) do
     params = Map.put(params, "scopes", "")
-    authorize(params)
+    authorize(conn, params)
   end
 
-  def authorize(params) do
+  def authorize(_conn, params) do
     reason = cond do
       params["email"] == nil -> "No email supplied"
       params["password"] == nil -> "No password supplied"
@@ -38,10 +42,10 @@ defmodule Tower.GrantType.Password do
     end
   end
 
-  def resource_owner_from_credentials(params) do
+  def resource_owner_from_credentials(conn, client) do
     case Application.get_env(:tower, :resource_owner_from_credentials) do
-      nil -> resource_owner_from_email_password(params)
-      ro -> ro.(params)
+      nil -> resource_owner_from_email_password(conn.params)
+      ro -> apply(ro, :resource_owner_from_credentials, [conn, client])
     end
   end
 
@@ -53,8 +57,12 @@ defmodule Tower.GrantType.Password do
 
   def create_access_token(client, owner_tuple, scopes \\ "")
   def create_access_token(_, {:error, reason}, _), do: {:error, reason}
-  def create_access_token(nil, _, _), do: {:error, "Invalid Client"}
   def create_access_token(client, {:ok, owner}, scopes) do
+    client_id = 
+      case is_nil(client) do
+        true -> nil
+        false -> client.id
+      end
     token_params = %{
                       resource_owner_id: owner.id,
                       application: client,
@@ -63,6 +71,6 @@ defmodule Tower.GrantType.Password do
                     }
 
     AccessTokenHelper.generate_token(token_params)
-    |> AccessTokenHelper.insert_access_token(resource_owner_id: owner.id, application_id: client.id, scopes: scopes, expires_in: token_params[:expires_in])
+    |> AccessTokenHelper.insert_access_token(resource_owner_id: owner.id, application_id: client_id, scopes: scopes, expires_in: token_params[:expires_in])
   end
 end
