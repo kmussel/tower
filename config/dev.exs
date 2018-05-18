c8cee8738460e2d0c5369ae1dd619f25e3a1c942
Jeremy Tregunna <jeremy@tregunna.ca>
2018-05-18 11:49:04 -0600

    grab pg user/pass from env

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 config :tower, Tower.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "argus",
-  password: "",
+  username: System.get_env("PG_USERNAME"),
+  password: System.get_env("PG_PASSWORD"),
   database: "metis_account",
   hostname: "localhost",
   pool_size: 10
